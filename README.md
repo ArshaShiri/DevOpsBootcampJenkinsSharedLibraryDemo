c381abe83701b44f1bb2ffa4b80ef2ea7c805722
ArshaShiri <arsha.shiri@gmail.com>
2022-12-25 12:08:07 +0100

    Updated readme and added some of the functions from java maven app demo project to the var directory.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-This is the shared library in Jenkins for DevOps boot camp project.
+# This is the shared library in Jenkins for DevOps boot camp project.
+
+## Project Structure
+
+**vars:** Functions that we call from Jenkinsfile. Each function/execution step is its own groovy file.
+
+**src:** helper code
+
+**resource:** use external libraries or non-groovy files.
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/env groovy
 
-import com.example.Docker
-
 def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    }
 }
