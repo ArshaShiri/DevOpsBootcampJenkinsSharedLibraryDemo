bd22a472c3bf5c831a00be784d337e1e3075f029
Arsha Shiri <arsha.shiri@gmail.com>
2022-12-27 19:12:10 +0100

    Further separate the logic under src.

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,14 +11,17 @@ class Docker implements Serializable {
     }
 
     def buildDockerImage(String imageName) {
-        script.echo "building the docker image..."
+        script.echo "Building the docekr image..."
         script.sh "docker build -t $imageName ."
-
-    script.echo "Building the docekr image..."
-    script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        script.sh "docker build -t $imageName ."
-        script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-        script.sh "docker push $imageName"
     }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        }
+    }
+
+    def dockerPuhs(String imageName) {
+        script.sh "docker push $imageName"
     }
 }
