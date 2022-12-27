34fad38d58960cb809d4fd8e3d62e35d074b298c
Arsha Shiri <arsha.shiri@gmail.com>
2022-12-27 16:52:05 +0100

    Replace with own version.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(String imageName) {
-    echo "building the docker image..."
+    echo "Building the docekr image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t arshashiri/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push arshashiri/demo-app:jma-2.0'
     }
 }
